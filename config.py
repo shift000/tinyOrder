0377cf34531e21375a77ab28169abe1d2c4d925c
shift <69255248+shift000@users.noreply.github.com>
2024-09-18 18:01:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    CLIENT_ID = os.getenv("CLIENT_ID", "b7a923cd-e2b2-44ac-94aa-266bd3aa8e5a")
-    CLIENT_SECRET = os.getenv("CLIENT_SECRET", "5f32fdc2-cf22-45ac-9a3c-5c42ec45a2be")
-    AUTHORITY = f"https://login.microsoftonline.com/{os.getenv('TENANT_ID', '5dde3815-af63-49d5-b149-e545b73512fd')}"
+    CLIENT_ID = os.getenv("CLIENT_ID", "<REDACTED>")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET", "<REDACTED>")
+    AUTHORITY = f"https://login.microsoftonline.com/{os.getenv('TENANT_ID', '<REDACTED>')}"
     REDIRECT_PATH = "/auth/callback"
     SCOPE = ["User.Read"]
     SESSION_TYPE = "filesystem"  # Zum Speichern der Sitzung
